f0dbdc64c76935dafeaae9713ab59c7eee267493
uvesh patel <uveshpatelgit@gmail.com>
2024-05-13 22:15:55 +0530

    home page

--- graphic_site/src/app/_component/Category.tsx
+++ graphic_site/src/app/_component/Category.tsx
@@ -1,19 +1,91 @@
+import { Bokor, Cardo } from 'next/font/google'
+import { title } from 'process'
 import React from 'react'
 
 const Category = () => {
+
+
+  const cardData =[{
+    image:'/media/graphic.jpg',
+    title:'Graphic Design',
+    description:'askdjkjdkslfjfsfd',
+    
+
+  },
+
+  {
+    image:'/media/graphic.jpg',
+    title:'Graphic Design',
+    description:'askdjkjdkslfjfsfd',
+    
+
+  },
+  {
+    image:'/media/graphic.jpg',
+    title:'Graphic Design',
+    description:'askdjkjdkslfjfsfd',
+    
+
+  },
+  {
+    image:'/media/graphic.jpg',
+    title:'Graphic Design',
+    description:'askdjkjdkslfjfsfd',
+    
+
+  },
+  {
+    image:'/media/graphic.jpg',
+    title:'Graphic Design',
+    description:'askdjkjdkslfjfsfd',
+    
+
+  },
+  {
+    image:'/media/graphic.jpg',
+    title:'Graphic Design',
+    description:'askdjkjdkslfjfsfd',
+    
+
+  },
+  {
+    image:'/media/graphic.jpg',
+    title:'Graphic Design',
+    description:'askdjkjdkslfjfsfd',
+    
+
+  },
+  {
+    image:'/media/graphic.jpg',
+    title:'Graphic Design',
+    description:'askdjkjdkslfjfsfd',
+    
+
+  },
+  {
+    image:'/media/graphic.jpg',
+    title:'Graphic Design',
+    description:'askdjkjdkslfjfsfd',
+    
+
+  },
+]
   return (
 <>
-<div className='bg-cyan-400 h-full p-24 glass'>
-<div className="card w-96  glass">
-  <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="car!"/></figure>
+<div className='bg-cyan-400  p-12 grid grid-cols-3  gap-4 glass'>
+  {cardData.map((data,index)=>(
+    <div key={index} className="card w-full p-0 glass">
+  <figure className='h-96'><img src={data.image} alt={data.image}/></figure>
   <div className="card-body">
-    <h2 className="card-title">Life hack</h2>
-    <p>How to park your car at your garage?</p>
+    <h2 className="card-title">{data.title}</h2>
+    <p>{data.description}</p>
     <div className="card-actions justify-end">
       <button className="btn btn-primary">Learn now!</button>
     </div>
   </div>
 </div>
+  ))}
+
 </div>
 
 </>
@@ -21,3 +93,15 @@ const Category = () => {
 }
 
 export default Category
+
+
+
+// Graphic Design
+// flex Banner
+// offset printing
+// screen printing 
+// wedding Card
+// visiting Card
+// bill Book 
+// letter head 
+// All kind of Files & bag Printing 
