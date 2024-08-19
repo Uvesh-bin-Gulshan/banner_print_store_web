016e5dfac0340130e3f998c46fa21be9528552d0
aamirbhai2003 <113134982+aamirbhai2003@users.noreply.github.com>
2024-08-19 19:52:54 +0530

    Update FlexBanner.tsx
    flex bannner

--- graphic_site/src/app/our-work/FlexBanner.tsx
+++ graphic_site/src/app/our-work/FlexBanner.tsx
@@ -6,26 +6,26 @@ const FlexBanner = () => {
   const cardData = [
     {
       image: '/media/flex1.jpg',
-      subtitle: 'SUBTITLE',
-      title: 'Chichen Itza',
+      subtitle: 'Custom Design Solutions',
+      title: 'Tailored to Your Brand’s Vision',
       description: 'Vibrant banners. Grab attention instantly.',
     },
     {
       image: '/media/flex2.jpeg',
-      subtitle: 'Flex Banner',
-      title: 'Colosseum Roma',
+      subtitle: 'Durable and Weather-Resistant',
+      title: 'Built to Last, Indoors and Out',
       description: 'Custom designs. Perfect for every event.',
     },
     {
       image: '/media/flex3.jpg',
-      subtitle: 'SUBTITLE',
-      title: 'sffsdf',
+      subtitle: 'High-Quality Printing',
+      title: 'Vibrant Colors and Crisp Details',
       description: 'Weather-resistant. Durable, long-lasting.',
     },
     {
       image: '/media/flex4.avif',
-      subtitle: 'SUBTITLE',
-      title: 'San Francisco',
+      subtitle: 'Custom Design Solutions',
+      title: 'Tailored to Your Brand’s Vision',
       description: 'Bold graphics. Convey your message clearly.',
     },
   ];
@@ -34,8 +34,7 @@ const FlexBanner = () => {
 
 <WorkSection className="bg-gray-500"
       title="Flex Banner"
-      description="Eye-Catching Banners
-Grab attention with custom-designed flex banners. Ideal for events, promotions, and storefronts."
+      description="At Faisal Graphics, we specialize in creating vibrant and durable flex banners that grab attention and convey your message with clarity. Whether you’re promoting an event, showcasing a product, or enhancing your storefront, our flex banners are designed to make a strong visual impact. With custom designs tailored to your brand and top-quality printing that ensures longevity, our banners are the perfect solution for any promotional need."
       cardData={cardData}
     />
 </>
