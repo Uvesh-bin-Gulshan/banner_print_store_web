0d013e7d581ad0b122da04f3cd09e73c0c9710c7
aamirbhai2003 <113134982+aamirbhai2003@users.noreply.github.com>
2024-08-19 20:31:38 +0530

    Update AboutIntro.tsx
    about us

--- graphic_site/src/app/about-contact-us/AboutIntro.tsx
+++ graphic_site/src/app/about-contact-us/AboutIntro.tsx
@@ -1,26 +1,20 @@
-import React from 'react'
+import React from 'react';
 
 const AboutIntro = () => {
   return (
-<>
-<section className="text-gray-600 text-center body-font">
-  <div className="container p-24 mx-auto">
-    <h2 className="sm:text-3xl text-2xl
-     text-gray-900 font-medium title-font mb-8 ">About Us</h2>
-    <div className=" text-center">
-      <p className="leading-relaxed text-base">
-        Our company was started in year 19YY. WE ARE SO AND SO . Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam iusto soluta provident, minima molestiae eum fuga nisi veritatis, nobis illum a ipsa at maxime, sint error nemo rem sunt. Asperiores.
-
-      </p>
-      
-    </div>
-  </div>
-</section>
-
-
-</> 
-
-)
+    <>
+      <section className="text-gray-600 text-center body-font">
+        <div className="container p-24 mx-auto">
+          <h2 className="sm:text-3xl text-2xl text-gray-900 font-medium title-font mb-8">About Us</h2>
+          <div className="text-center">
+            <p className="leading-relaxed text-base">
+              Founded in 2001, Faisal Graphics has been at the forefront of the printing and design industry for over two decades. Our journey began with a passion for creativity and a commitment to quality, and we have since grown into a trusted name for all your graphic and printing needs. From business cards and wedding invitations to custom bill printing and promotional materials, we pride ourselves on delivering exceptional products that enhance your brand's identity. At Faisal Graphics, we blend tradition with innovation to bring your vision to life with unmatched precision and artistry.
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default AboutIntro
+export default AboutIntro;
