9f61e5f08d071851b93321f73f9e2d10009d8833
aamirbhai2003 <113134982+aamirbhai2003@users.noreply.github.com>
2024-08-23 18:26:22 +0530

    Update VisitingCard.tsx
    visiting card images set

--- graphic_site/src/app/our-work/VisitingCard.tsx
+++ graphic_site/src/app/our-work/VisitingCard.tsx
@@ -5,25 +5,25 @@ const VisitingCard = () => {
 
   const cardData = [
     {
-      image: 'https://dummyimage.com/720x400',
+      image: '/media/Visiting Card  1.jpg',
       subtitle: 'Custom Designs Tailored to You',
       title: 'Unique and Memorable Business Cards',
       description: 'Stand out with the bespoke business cards that reflect your brand’s identity. Whether you need a minimalist design or something bold and eye-catching, our custom designs ensure that your business cards leave a lasting impression.',
     },
     {
-      image: 'https://dummyimage.com/721x401',
+      image: '/media/Visiting Card  2.jpg',
       subtitle: 'Premium Finishes for a Professional Look',
       title: 'Enhance Your Cards with High-Quality Finishes',
       description: 'Choose from a variety of premium finishes such as UV coating, embossing, gold foil, and velvet touch. These options add a layer of sophistication and durability to your business cards, making them not only visually appealing but also long-lasting.',
     },
     {
-      image: 'https://dummyimage.com/722x402',
+      image: '/media/Visiting Card  3.jpg',
       subtitle: 'Durable and Versatile Options',
       title: 'Business Cards in All Shapes and Sizes',
       description: 'We offer business cards in various shapes and sizes, including die-cut options. Whether you prefer standard dimensions or a custom shape that stands out, we ensure your cards are as unique as your brand.',
     },
     {
-      image: 'https://dummyimage.com/722x402',
+      image: '/media/Visiting Card  4jpg.jpg',
       subtitle: 'Affordable and Reliable Service',
       title: 'High-Quality Printing at Competitive Prices',
       description: 'At Faisal Graphics, we combine quality with affordability. Our reliable printing services ensure that you receive high-quality business cards on time and within budget, making us the perfect choice for all your printing needs.',
